58ed465b683d21bf528a04a510a70e37cf83d31f
Paddsladd <142200743+Paddsladd@users.noreply.github.com>
2023-08-16 19:57:33 +0000

    Updated start.bat

--- start.bat
+++ start.bat
@@ -13,8 +13,8 @@ echo \____________________________/
 echo Server is starting...
 start "C:\XboxGames\Minecraft Launcher\Content\Minecraft.exe" "C:\XboxGames\Minecraft Launcher\Content\Minecraft.exe"
 java -Xmx1024M -Xms1024M -jar server.jar nogui
-color 07
 :a
+color 07
 cls
 echo [1] Close Terminal
 echo [2] Restart Server
@@ -22,6 +22,7 @@ echo [3] Open log file
 set /p ans="Enter a number:"
 if %ans%==1 (
 start notepad.exe "%userprofile%\Documents\Minecraft server (Java)\logs\latest.log"
+exit
 )
 if %ans%==2 (
 cls
@@ -31,3 +32,8 @@ if %ans%==3 (
 start notepad.exe "%userprofile%\Documents\Minecraft server (Java)\logs\latest.log"
 goto a
 )
+cls
+color 04
+echo "%ans%" is not a valid number!
+timeout /t 2 /nobreak >nul
+goto a
